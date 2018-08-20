432f1f3f1113e5657b3f2230013cc2dd1093324f
Grayfield <grayfieldmm@outlook.com>
2018-08-20 09:56:19 +0700

    create item

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const routers = require('./routers')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+mongoose.connect('mongodb://127.0.0.1/item-storage', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('db connected!')
+});
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
--- server/controllers/items.js
+++ server/controllers/items.js
@@ -1,9 +1,19 @@
+const Item = require('../models/item')
+
 module.exports = {
   search: (req, res, next) => {
     res.send('search item')
   },
 
   create: (req, res, next) => {
-    res.send('create item')
+    const { name, price, stock, tags } = req.body
+
+    Item.create({ name, price, stock, tags })
+      .then(new_item => {
+        res.status(201).json(new_item.getInfo())
+      })
+      .catch(err => {
+        res.status(400).json({ message: 'fail create item' })
+      })
   }
 }
